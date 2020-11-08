dfbf629205d56eec6aecd9a24073ac37c10b6542
jieblue <53696774+jieblue@users.noreply.github.com>
2020-11-08 10:21:02 +0800

    添加单元测试

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -2,24 +2,51 @@ package com.example.demo;
 
 import com.example.controller.HtmlController;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
 
 import static org.hamcrest.CoreMatchers.*;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@AutoConfigureMockMvc
 public class DemoApplicationTests {
 
     @Autowired
     private HtmlController htmlController;
 
-    @Test
-    public void getLearn(){
+    @Autowired
+    private WebApplicationContext webApplicationContext;
 
-
+    private MockMvc mockMvc;
+    @Before
+    public void setup()
+    {
+        this.mockMvc= MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
     }
+    @Test
+    public void getcountresult() throws Exception{
+
+        String uri = "/Getcount";
+
+
+        // 模拟GET请求
+        MvcResult mvcResult = this.mockMvc
+                .perform(MockMvcRequestBuilders.post(uri))
+                .andReturn();// 获取返回结果
+
+        // 打印返回结果
+        System.out.println(mvcResult.getResponse().getContentAsString());
+    }
+
 }
